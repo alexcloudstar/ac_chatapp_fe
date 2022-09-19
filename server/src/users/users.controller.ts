f4e54cce91388a1067c0638d551ad34ed59ddd2e
alexcloudstar <alexcloudstar@gmail.com>
2022-09-19 09:38:56 +0300

    Tweak User methods && Added Custom exception

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/user-update.dto';
 import { UserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
 
@@ -15,8 +25,29 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @Get('/:id')
+  async findOne(@Param('id') id: string): Promise<User> {
+    try {
+      const user = await this.usersService.find(parseInt(id));
+
+      return user;
+    } catch (error) {
+      throw new NotFoundException(`User not found with given id: ${id}`);
+    }
+  }
+
   @Post()
   create(@Body() body: CreateUserDto): Promise<User> {
     return this.usersService.create(body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
+import { CustomException } from '../exceptions/custom-exception';
+import { UpdateUserDto } from './dto/user-update.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
+  find(id: number): Promise<User> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
 
   findAll(): Promise<User[]> {
     return this.prisma.user.findMany();
@@ -19,5 +25,31 @@ export class UsersService {
         admin: false,
       },
     });
+  }
+
+  async remove(id: number): Promise<string> {
+    try {
+      await this.prisma.user.delete({ where: { id } });
+
+      return 'User deleted succesfuly';
+    } catch (error) {
+      throw new NotFoundException('User not found');
+    }
+  }
+
+  async update(id: number, body: UpdateUserDto) {
+    try {
+      const user = await this.prisma.user.update({ where: { id }, data: body });
+
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new CustomException(
+          'This username is already in use',
+          HttpStatus.UNPROCESSABLE_ENTITY,
+        );
+
+      throw new NotFoundException('User not found');
+    }
   }
 }
