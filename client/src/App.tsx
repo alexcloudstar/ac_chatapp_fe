0fb06c4962eeeddff31d18d9efa13a5de7377a09
alexcloudstar <alexcloudstar@gmail.com>
2022-10-14 14:55:22 +0300

    Fixing eslintrc errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,20 +6,6 @@ function App() {
     <main className={styles.container}>
       <ChatHeader />
     </main>
-    // <div className="container w-full max-w-full flex flex-col justify-center items-center h-full">
-    //   <h1 className="text-3xl font-bold underline text-red-500 mb-8">
-    //     Hello world!
-    //   </h1>
-
-    //   <Button
-    //     label="Click me"
-    //     backgroundColor="red"
-    //     primary={true}
-    //     key="exampleBtn"
-    //     onClick={() => console.log('I was clicked')}
-    //     size="large"
-    //   />
-    // </div>
   )
 }
 
