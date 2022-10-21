19289d16b9a3127afbfb6786c777d58e4060c60d
alexcloudstar <alexcloudstar@gmail.com>
2022-10-22 01:24:29 +0300

    Adding API for chatrooms

--- client/src/components/ChatList/ChatList.tsx
+++ client/src/components/ChatList/ChatList.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import styles from './chatlist.module.css'
 import { Preview } from './components'
 
+import { User } from '@/stories/types'
+import { API_METHODS } from '@/types'
+import { fetchAPI } from '@/utils/api'
+import { getLocalStorage } from '@/utils/localStorage'
+
 const ChatList = () => {
+  const [conversations, setConversations] = useState<any[]>([])
+
+  const getChatrooms = useCallback(async () => {
+    const APIData: any[] = await fetchAPI(
+      'http://localhost:4000/chatrooms/joined',
+      API_METHODS.GET,
+      getLocalStorage('accessToken') || ''
+    )
+
+    setConversations(APIData)
+  }, [])
+
+  useEffect(() => {
+    getChatrooms()
+  }, [getChatrooms])
+
+  console.log(conversations)
+
   return (
     <div className={`${styles.container}`}>
       <Preview />
