ff0d9f02b7978b9ad95786084d71612f3f800408
alexcloudstar <alexcloudstar@gmail.com>
2022-10-22 00:25:32 +0300

    Implementing Users API List into Rooms

--- client/src/components/Rooms/Rooms.tsx
+++ client/src/components/Rooms/Rooms.tsx
@@ -1,10 +1,16 @@
+import { useCallback, useEffect, useState } from 'react'
 import { SwiperSlide } from 'swiper/react'
 
 import { RoomsSlider } from './components'
 
 import { Room } from '@/stories'
+import { User } from '@/stories/types'
+import { API_METHODS } from '@/types'
+import { fetchAPI } from '@/utils/api'
+import { getLocalStorage } from '@/utils/localStorage'
 
 const Rooms = () => {
+  const [users, setUsers] = useState<User[]>([])
   const user = {
     firstName: 'John',
     lastName: 'Doe',
@@ -12,56 +18,36 @@ const Rooms = () => {
       'https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Prescription02&facialHairType=BeardMedium&facialHairColor=BlondeGolden&clotheType=ShirtScoopNeck&clotheColor=Red&eyeType=Close&eyebrowType=UpDownNatural&mouthType=Smile&skinColor=Pale',
   }
 
+  const getUsers = useCallback(async () => {
+    const APIData: User[] = await fetchAPI(
+      'http://localhost:4000/users/',
+      API_METHODS.GET,
+      getLocalStorage('accessToken') || ''
+    )
+
+    setUsers(APIData.filter((user) => user.id !== 1)) // TODO: implement redux toolkit
+  }, [])
+
+  useEffect(() => {
+    getUsers()
+  }, [getUsers])
+
   return (
     <>
       <h2 className="mt-[25px] mb-[15px]">Chatrooms</h2>
       <div>
         <RoomsSlider>
-          <SwiperSlide>
-            <Room
-              owner={`${user.firstName} ${user.lastName}`}
-              isFavorite
-              classes="mr-[15px]"
-              bgImage={user.avatar}
-              onClick={() => console.log('Room 1 clicked')}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Room
-              owner={`Jane Doe`}
-              isFavorite
-              classes="mr-[15px]"
-              bgImage={user.avatar}
-              onClick={() => console.log('Room 2 clicked')}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Room
-              owner={`user 1`}
-              isFavorite
-              classes="mr-[15px]"
-              bgImage={user.avatar}
-              onClick={() => console.log('Room 3 clicked')}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Room
-              owner={`user 2`}
-              isFavorite
-              classes="mr-[15px]"
-              bgImage={user.avatar}
-              onClick={() => console.log('Room 4 clicked')}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Room
-              owner={`${user.firstName} ${user.lastName}`}
-              isFavorite
-              classes="mr-[15px]"
-              bgImage={user.avatar}
-              onClick={() => console.log('Room 5 clicked')}
-            />
-          </SwiperSlide>
+          {users.map((user) => (
+            <SwiperSlide key={user.id}>
+              <Room
+                owner={user.username}
+                isFavorite={false}
+                classes="mr-[15px]"
+                bgImage={user.avatar}
+                onClick={() => console.log('Room 1 clicked')}
+              />
+            </SwiperSlide>
+          ))}
         </RoomsSlider>
       </div>
     </>
