84a85dd02a2e2f7fbb48cec06a5054332001c2a1
alexcloudstar <alexcloudstar@gmail.com>
2022-10-26 14:35:32 +0300

    Implement bg pattern & other UI Tweaks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       {!isLoggedIn ? (
         <Auth setIsLoggedIn={setIsLoggedIn} />
       ) : (
-        <main className="flex flex-col w-[90%] lg:w-[70%]  h-[90%]	 pt-[50px] pr-[30px] pl-[30px] pb-[30px] text-white bg-[#596787] rounded-[40px]">
+        <main className="flex flex-col w-[90%] lg:w-[70%]  h-[90%]	 pt-[50px] pr-[30px] pl-[30px] pb-[30px] text-white bg-[#596787]/[70%] rounded-[40px] shadow-floating-container">
           <ChatHeader setIsLoggedIn={setIsLoggedIn} />
           <Rooms />
           <ChatList />
