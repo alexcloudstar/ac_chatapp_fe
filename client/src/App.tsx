103fcc6f3e2ea18225ee3550b26f6619a2563c91
alexcloudstar <alexcloudstar@gmail.com>
2022-10-26 15:46:09 +0300

    Fix UI for App & Storybook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,17 +8,19 @@ function App() {
   )
 
   return (
-    <>
-      {!isLoggedIn ? (
-        <Auth setIsLoggedIn={setIsLoggedIn} />
-      ) : (
-        <main className="flex flex-col w-[90%] lg:w-[70%]  h-[90%]	 pt-[50px] pr-[30px] pl-[30px] pb-[30px] text-white bg-[#596787]/[70%] rounded-[40px] shadow-floating-container">
-          <ChatHeader setIsLoggedIn={setIsLoggedIn} />
-          <Rooms />
-          <ChatList />
-        </main>
-      )}
-    </>
+    <div className="app-root">
+      <div className="app--inner-root">
+        {!isLoggedIn ? (
+          <Auth setIsLoggedIn={setIsLoggedIn} />
+        ) : (
+          <main className="flex flex-col w-[90%] lg:w-[70%]  h-[90%]	 pt-[50px] pr-[30px] pl-[30px] pb-[30px] text-white bg-[#596787]/[70%] rounded-[40px] shadow-floating-container">
+            <ChatHeader setIsLoggedIn={setIsLoggedIn} />
+            <Rooms />
+            <ChatList />
+          </main>
+        )}
+      </div>
+    </div>
   )
 }
 
