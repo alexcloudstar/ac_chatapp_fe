61422a83815b2523a104324d492c3879f1a74f86
alexcloudstar <alexcloudstar@gmail.com>
2022-11-01 13:05:11 +0200

    Implement React Select & Tweak UI

--- client/src/components/CreateRoom/CreateRoom.tsx
+++ client/src/components/CreateRoom/CreateRoom.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import Select from 'react-select'
 
 import { Toggle } from '@/stories/components'
 
@@ -23,15 +24,28 @@ const CreateRoom = () => {
 
   const onSubmit: SubmitHandler<CreateRoomFormInputs> = async (data) => {}
 
+  const colourOptions = [
+    { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+    { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
+    { value: 'purple', label: 'Purple', color: '#5243AA' },
+    { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
+    { value: 'orange', label: 'Orange', color: '#FF8B00' },
+    { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+    { value: 'green', label: 'Green', color: '#36B37E' },
+    { value: 'forest', label: 'Forest', color: '#00875A' },
+    { value: 'slate', label: 'Slate', color: '#253858' },
+    { value: 'silver', label: 'Silver', color: '#666666' },
+  ]
+
   return (
     <form
       action="POST"
       onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col justify-center"
     >
-      <div className="w-100 flex flex-col">
-        <div className="flex w-100">
-          <div className="w-100">
+      <div className="w-full flex flex-col">
+        <div className="flex w-full">
+          <div className="w-full">
             <input
               type="text"
               className="mt-2 outline-0 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
@@ -44,7 +58,7 @@ const CreateRoom = () => {
             )}
           </div>
 
-          <div className="w-100">
+          <div className="w-full">
             <input
               type="text"
               className="mt-2 outline-0 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
@@ -56,20 +70,24 @@ const CreateRoom = () => {
               <p className="mt-5 mb-5 text-red-500">This field is required</p>
             )}
           </div>
-
-          <div className="w-100">
-            <select>
-              <option value="volvo">Volvo</option>
-              <option value="saab">Saab</option>
-              <option value="mercedes">Mercedes</option>
-              <option value="audi">Audi</option>
-            </select>
-          </div>
         </div>
 
-        <div className="flex justify-between items-center mt-5">
-          <p className="text-gray-900 dark:text-gray-100">Private</p>
-          <Toggle isOn={isPrivate} setIsOn={setIsPrivate} />
+        <div className="flex w-full">
+          <div className="w-full">
+            <Select
+              defaultValue={[colourOptions[2], colourOptions[3]]}
+              isMulti
+              name="colors"
+              options={colourOptions}
+              className="basic-multi-select"
+              classNamePrefix="select"
+            />
+          </div>
+
+          <div className="flex justify-between items-center mt-5">
+            <p className="text-gray-900 dark:text-gray-100">Private</p>
+            <Toggle isOn={isPrivate} setIsOn={setIsPrivate} />
+          </div>
         </div>
       </div>
 
