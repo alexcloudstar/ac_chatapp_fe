62f9a5160494be1ca6f42ad35532fe9e08e21cb7
alexcloudstar <alexcloudstar@gmail.com>
2022-11-02 22:07:19 +0200

    Create not Found route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,5 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Router } from './routes'
 
-import { ChatHeader, ChatList, Rooms } from './components'
-import { HeaderLayout } from './Layouts'
-
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(
-    !!localStorage.getItem('accessToken')
-  )
-
-  return (
-    <BrowserRouter>
-      <HeaderLayout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
-        <ChatHeader setIsLoggedIn={setIsLoggedIn} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Rooms />
-                <ChatList />
-              </>
-            }
-          />
-        </Routes>
-      </HeaderLayout>
-    </BrowserRouter>
-  )
-}
+const App = () => <Router />
 
 export default App
