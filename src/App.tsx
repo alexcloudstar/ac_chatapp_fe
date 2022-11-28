811cd267c268143bc57c3185fe8c82750551f2a6
alexcloudstar <alexcloudstar@gmail.com>
2022-11-28 15:05:33 +0200

    Remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { API_URL } from 'config/env';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { routes } from './routes';
 
 const App = () => {
-  console.log(API_URL);
-  console.log(process.env.API_URL);
+
   return (
     <main className='flex flex-col justify-center items-center w-full h-full bg-[#596787]/[70%]'>
       <BrowserRouter>
