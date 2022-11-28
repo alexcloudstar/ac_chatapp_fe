b84e3a61096d6122bc189e6addee7884e58f9e55
alexcloudstar <alexcloudstar@gmail.com>
2022-11-28 13:57:44 +0200

    add console.log for API URL

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,1 @@
-export const API_URL =
-  (import.meta.env.VITE_API_URL as string) ||
-  (process.env.VITE_API_URL as string) ||
-  (process.env.REACT_APP_API_URL as string) ||
-  'http://localhost:8000';
+export const API_URL = import.meta.env.VITE_API_URL as string;
