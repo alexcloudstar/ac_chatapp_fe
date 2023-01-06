e4d2fb6db5451d7b7dfaef8a1628bbfc203581ce
alexcloudstar <alexcloudstar@gmail.com>
2023-01-06 22:08:13 +0200

    Fix profanity words on homepage

--- src/components/ChatList/ChatList.tsx
+++ src/components/ChatList/ChatList.tsx
@@ -40,6 +40,17 @@ const ChatList = () => {
           const lastMessage =
             conversation.messages[conversation.messages.length - 1]
 
+          const isProfanityWord = conversation?.profanityWords.some(
+            (pWord) => pWord === lastMessage.message
+          )
+
+          const checkedMessage = isProfanityWord
+            ? {
+                ...lastMessage,
+                message: '****',
+              }
+            : lastMessage
+
           const lastMessageTime = new Date(
             lastMessage.createdAt
           ).toLocaleString('ro-RO', {
@@ -61,7 +72,7 @@ const ChatList = () => {
                 }}
                 message={
                   conversation.messages.length
-                    ? lastMessage?.message
+                    ? checkedMessage.message
                     : 'No message yet'
                 }
                 time={lastMessageTime}
