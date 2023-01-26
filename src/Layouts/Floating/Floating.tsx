55f1cdcb86fd5b5b3355b8275b699c08aac40b67
alexcloudstar <alexcloudstar@gmail.com>
2023-01-26 21:09:27 +0200

    fix: Mobile UI

--- src/Layouts/Floating/Floating.tsx
+++ src/Layouts/Floating/Floating.tsx
@@ -12,7 +12,7 @@ const Floating: FC<{ children: React.ReactNode }> = ({ children }) => {
 
   return (
     <div
-      className={`flex flex-col w-[90%] lg:w-[70%]  h-[90%]	 pt-[50px] pr-[30px] pl-[30px] pb-[30px] text-white bg-[#596787]/[70%] rounded-[40px] shadow-floating-container relative ${includeOverflow}`}
+      className={`flex flex-col w-[90%] lg:w-[70%] h-[70%] sm:h-[90%] pt-[50px] pr-[30px] pl-[30px] pb-[30px] text-white bg-[#596787]/[70%] rounded-[40px] shadow-floating-container relative ${includeOverflow}`}
     >
       <ChatHeader />
       {children}
